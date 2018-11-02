81ec4a0d3db2ac6068fffefef95f56dfb2708ec0
RVIRUS0817 <t.virus.1989@gmail.com>
2018-11-02 10:48:21 +0900

    fix waf

--- lambda_function.py
+++ lambda_function.py
@@ -9,20 +9,17 @@ from geoip import geolite2
 def reverse_dns(ip):
     result_ip = re.sub('\/.*',"",ip)
     result_addr = reversename.from_address(result_ip)
-    resolver.timeout = 30
-    resolver.lifetime = 30
     result_record = resolver.query(result_addr, "PTR")[0]
     return result_record
 
-def notification_slack(ip):
+def notification_slack(ip, result_record):
     url = os.environ.get('SLACK_WEBHOOK_URL')
     title = ':warning:  本番ALBでDoS攻撃を検知しました！！！:warning: '
-    text = "遮断したアクセス元 ```%s\n``` " % (ip)
+    text = "遮断したアクセス元 ```%s\n%s``` " % (ip, result_record)
     color = 'danger'
-
-    payload = {
+     payload = {
         'channel': 'bot_alerts',
-        'username': 'lancers-waf',
+        'username': 'waf',
         'attachments': [
             {
                 'fallback': title + ' - ' + text,
@@ -32,18 +29,18 @@ def notification_slack(ip):
             }
         ]
     }
-
-    headers = {'content-type': 'application/json'}
-    requests.post(url, data=json.dumps(payload), headers=headers)
+     headers = {'content-type': 'application/json'}
+     requests.post(url, data=json.dumps(payload), headers=headers)
 
 def lambda_handler(event, context):
     ruleid = os.environ.get('RULEID')
     client = boto3.client('waf-regional')
     response = client.get_rate_based_rule_managed_keys(
-       RuleId=ruleid
+    RuleId=ruleid
     )
     dict_ip = response.get("ManagedKeys")
     for ip in dict_ip:
         print(ip)
-        notification_slack(ip)
+        result_record = reverse_dns(ip)
+        notification_slack(ip, result_record)
 
